fda041119d5c43be0c7c6fe69630c5cb8f022dc0
Pooya Nazari <66327968+pooyanazarii@users.noreply.github.com>
2022-09-20 20:58:48 +0430

    Added form maindesk

--- project/rc_library/rc_library/Form1.vb
+++ project/rc_library/rc_library/Form1.vb
@@ -1,4 +1,7 @@
 ﻿Public Class input_form
 
 
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        maindesk.Show()
+    End Sub
 End Class
---
+++ project/rc_library/rc_library/maindesk.vb
@@ -0,0 +1,7 @@
+﻿Public Class maindesk
+
+
+    Private Sub maindesk_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        input_form.Hide()
+    End Sub
+End Class
